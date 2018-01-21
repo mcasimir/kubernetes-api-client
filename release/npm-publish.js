2a1e8a56b2a593c824eaad13d4f005c12b3c2ba2
mcasimir <maurizio.cas@gmail.com>
2018-01-21 23:21:54 +0100

    add debug info to request errors

--- release/npm-publish.js
+++ release/npm-publish.js
@@ -1,7 +1,7 @@
 const {execSync} = require('child_process');
 
 module.exports = function installPlugin(release) {
-  release.phases.finish.push({
+  release.phases.finish.steps.push({
     name: 'npmPublish',
     run() {
       execSync('npm publish');
